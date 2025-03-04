b51d975d899f66f4a4d0dc85dc1b4f6a90bc4369
Eyob Mekonnen <emekonnen35@gmail.com>
2025-03-04 10:44:46 +0300

    last push

--- index.js
+++ index.js
@@ -15,31 +15,17 @@ const app = express();
 
 // Connect to the database
 connectDB();
-
-const allowedOrigins = [
-  "https://ticketing-system-henna.vercel.app",
-  "http://localhost:5173",
-];
-
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (allowedOrigins.includes(origin)) {
-        console.log(`CORS request allowed from: ${origin}`);
-        callback(null, true); // Allow the origin
-      } else {
-        console.log(`CORS request denied from: ${origin}`); // Log denied origin
-        callback(new Error("Not allowed by CORS")); // Reject other origins
-      }
-    },
+    origin: "*", // Allow all origins
     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
   })
 );
 
-
+// Enable CORS with the specified options
 // app.use(cors(corsOptions));
-app.use(bodyParser.json()); 
+app.use(bodyParser.json()); // To parse JSON bodies
 app.use(loggerMiddleware);
 app.use("/uploads", express.static("uploads"));
 // Middleware
